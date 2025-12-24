52cda2967956cc67e80cc1f2f2874e21746beafa
sanjeta-singh <sanjetasingh688@gmail.com>
2025-12-24 20:04:46 +0530

    subnet added

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,4 +16,22 @@ provider "azurerm" {
   features{}
 }
 
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+  location = var.resource_group_location
+}
 
+resource "azurerm_virtual_network" "vnet_dev" {
+  name = var.vnet_cidr_name
+  address_space = var.vnet_cidr_address_space
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet_dev" {
+  name = var.subnet_name
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet_dev.name
+  address_prefixes = var.subnet_address_space
+
+}
