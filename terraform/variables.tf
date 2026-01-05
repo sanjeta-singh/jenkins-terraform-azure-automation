905e31724e48c0cecedd0daa1a792505c4c6d9c8
sanjeta-singh <sanjetasingh688@gmail.com>
2026-01-05 23:51:01 +0530

    fix wrong variable filename

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,7 @@ variable "resource_group_name" {
 
 variable "resource_group_location" {
     type = string
-    default = "eastus"
+    default = "westus"
     description = "Location of the Resource Group"
 }
 
@@ -40,6 +40,8 @@ variable "subnet_name" {
     description = "Name of the Subnet"
 }
 
-variable "subnet_addr" {
-  
+variable "subnet_address_space" {
+    type = list(string)
+    default = ["10.0.1.0/24"]
+    description = "CIDR block for the Subnet"
 }
