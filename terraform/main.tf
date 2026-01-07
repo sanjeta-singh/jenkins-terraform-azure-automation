202286f374f0a02eea1f57afe0f0eb6b791c9ff3
sanjeta-singh <sanjetasingh688@gmail.com>
2026-01-07 23:57:41 +0530

    adding resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,110 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.0"
+  terraform {
+    required_providers {
+      azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~>4.0"
+      }
+      random = {
+        source  = "hashicorp/random"
+        version = "~>3.0"
+      }
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
+  }
+
+
+  provider "azurerm" {
+    features{} # in features block azure provider settings can be defined such as enabling/disabling key vaults or other services
+  }
+
+  resource "azurerm_resource_group" "rg" {
+    name = var.resource_group_name
+    location = var.resource_group_location
+  }
+
+  resource "azurerm_virtual_network" "vnet_dev" {
+    name = var.vnet_cidr_name
+    address_space = var.vnet_cidr_address_space
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+  }
+
+  resource "azurerm_subnet" "subnet_dev" {
+    name = var.subnet_name
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet_dev.name
+    address_prefixes = var.subnet_address_space
+
+  }
+   
+   
+  resource "azurerm_public_ip" "pip" {
+   name                = var.public_ip_name
+   resource_group_name = azurerm_resource_group.rg.name
+   location            = azurerm_resource_group.rg.location
+   allocation_method   = "Static"
+   sku                 = "Standard"
+  }
+
+  resource "azurerm_network_security_group" "nsg" {
+   name                = "jenkins-nsg"
+   location            = azurerm_resource_group.rg.location
+   resource_group_name = azurerm_resource_group.rg.name
+   security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+   } 
+  }
+
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet_dev.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
+ resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+  } 
 
-provider "azurerm" {
-  features{}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
-  location = var.resource_group_location
-}
-
-resource "azurerm_virtual_network" "vnet_dev" {
-  name = var.vnet_cidr_name
-  address_space = var.vnet_cidr_address_space
-  location = azurerm_resource_group.rg.location
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
-}
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username_vm
+  
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
 
-resource "azurerm_subnet" "subnet_dev" {
-  name = var.subnet_name
-  resource_group_name = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet_dev.name
-  address_prefixes = var.subnet_address_space
+  admin_ssh_key {
+    username   = var.admin_username_vm
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
